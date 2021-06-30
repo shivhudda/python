c51696ae31b01894f49c3623b26cf865b6761a4e
shivhudda <shivhudda072@gmail.com>
2021-06-30 15:35:47 +0530

    lists

---
+++ 10_list.py
@@ -0,0 +1,17 @@
+list1 = ['ram', 23,True,3+4j , 'shivhudda' ,45.3]
+
+# swapping two numbers
+a = 10
+b = 34
+a,b = b,a
+print(a,b)
+
+l1 = ['ram', 23,True,3+4j , 'shivhudda' ,45.3]
+print(l1[3])   #finding index characters
+
+l1.remove('ram')  #remove an element
+
+l1.pop(-1)  #pop a item using index numbers
+
+l1 = ['shiv']
+print(l1)
