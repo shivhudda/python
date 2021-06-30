5d4056fe0529f8085568b1fbbf08c2afd62555af
shivhudda <82936721+shivhudda@users.noreply.github.com>
2021-06-30 10:38:00 +0530

    seq. file

---
+++ 7_Escape_Sequences.py
@@ -0,0 +1,23 @@
+print('hi,\n we are learning python.')
+# Inserts a new line in the text at the point using \n
+
+print("a\\b")
+	# Inserts a backslash character in the text at the point using \\
+
+print("\"Hello World\"")
+# Inserts a double quote character in the text at that point using \"
+
+print('\'hello world\'')
+# Inserts a single quote character in the text at that point using \'
+
+print('hey,\t come here.')
+# Inserts a tab in the text at that point using \t
+
+print('hello world:\fthis is my first program. ')
+# Inserts a form feed ln the text at that point using \f
+
+print('this is a \r python program.')
+# Inserts a carriage return in the text at that point using \r
+
+print('hi, \bhello world')
+# Inserts a backspace in the text at that point using \b
